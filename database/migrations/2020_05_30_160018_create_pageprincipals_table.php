5616539e873bc4ed3758e229f2d197d05f1ed8e3
ghsilva-ops <58492128+ghsilva-ops@users.noreply.github.com>
2020-06-02 19:44:50 -0300

    up

--- database/migrations/2020_05_30_160018_create_pageprincipals_table.php
+++ database/migrations/2020_05_30_160018_create_pageprincipals_table.php
@@ -20,28 +20,7 @@ class CreatePageprincipalsTable extends Migration
             $table->longText('base64_photo_principal')->nullable();
             $table->string('text_welcome_title')->nullable();
             $table->string('text_welcome')->nullable();
-            $table->longText('base64_photo1')->nullable();
-            $table->longText('base64_photo2')->nullable();
-            $table->longText('base64_photo3')->nullable();
-            $table->longText('base64_photo4')->nullable();
-            $table->longText('base64_photo5')->nullable();
-            $table->longText('base64_photo6')->nullable();
-            $table->longText('base64_photo7')->nullable();
-            $table->longText('base64_photo8')->nullable();
-            $table->longText('base64_photo9')->nullable();
-            $table->longText('base64_photo10')->nullable();
-            $table->longText('base64_photo11')->nullable();
-            $table->longText('base64_photo12')->nullable();
-            $table->longText('base64_photo13')->nullable();
-            $table->longText('base64_photo14')->nullable();
-            $table->longText('base64_photo15')->nullable();
-            $table->longText('base64_photo_coments1')->nullable();
-            $table->longText('base64_photo_coments2')->nullable();
-            $table->longText('base64_destack1')->nullable();
-            $table->longText('base64_destack2')->nullable();
-            $table->longText('base64_destack3')->nullable();
-            $table->longText('base64_destack4')->nullable();
-
+            $table->longText('base64_photo')->nullable();
             $table->timestamps();
         });
     }
@@ -58,25 +37,9 @@ class CreatePageprincipalsTable extends Migration
             $table->string('text_perfil')->nullable();
             $table->longText('base64_photo_perfil')->nullable();
             $table->longText('base64_photo_principal')->nullable();
-            $table->longText('base64_photo1')->nullable();
-            $table->longText('base64_photo2')->nullable();
-            $table->longText('base64_photo3')->nullable();
-            $table->longText('base64_photo4')->nullable();
-            $table->longText('base64_photo5')->nullable();
-            $table->longText('base64_photo6')->nullable();
-            $table->longText('base64_photo7')->nullable();
-            $table->longText('base64_photo8')->nullable();
-            $table->longText('base64_photo9')->nullable();
-            $table->longText('base64_photo10')->nullable();
-            $table->longText('base64_photo11')->nullable();
-            $table->longText('base64_photo12')->nullable();
-            $table->longText('base64_photo13')->nullable();
-            $table->longText('base64_photo14')->nullable();
-            $table->longText('base64_photo15')->nullable();
-            $table->longText('base64_semana1')->nullable();
-            $table->longText('base64_semana2')->nullable();
-            $table->longText('base64_semana3')->nullable();
-            $table->longText('base64_semana4')->nullable();
+            $table->string('text_welcome_title')->nullable();
+            $table->string('text_welcome')->nullable();
+            $table->longText('base64_photo')->nullable();
         });
     }
 }
