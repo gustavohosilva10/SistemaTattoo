f40365d4a0f5c9b24debd51e92893be8d76323d3
ghsilva-ops <58492128+ghsilva-ops@users.noreply.github.com>
2020-07-02 11:55:26 -0300

    up

--- app/Pageprincipals.php
+++ app/Pageprincipals.php
@@ -7,13 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Pageprincipals extends Model
 {
     public $fillable = [
-    'id',
-    'text_perfil',
-    'base64_photo_perfil',
-    'base64_photo_principal',
-    'base64_photo',
-    'text_welcome_title',
-     'base64_photo_coments1',
-     'base64_photo_coments2'
+        'base64_photo_principal',
+        'text_welcome_title',
+        'text_welcome',
+        'text_perfil',
+        'base64_image1',
+        'base64_image2',
+        'base64_image3',
+        'base64_image4',
+        'base64_image5',
+        'base64_image6',
+        'base64_image7',
+        'base64_image8',
+        'base64_image9',
+        'base64_image10',
+        'base64_image11',
+        'base64_image12',
+        'base64_photo_perfil',
+        'base64_photo',
+   
      ];
 }
