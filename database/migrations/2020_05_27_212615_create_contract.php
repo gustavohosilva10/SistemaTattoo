9e3dae68480ee2404fc502eef98cfa4d63bfbc71
ghsilva-ops <58492128+ghsilva-ops@users.noreply.github.com>
2020-08-20 12:00:05 -0300

    up

--- database/migrations/2020_05_27_212615_create_contract.php
+++ database/migrations/2020_05_27_212615_create_contract.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateContract extends Migration
 {
@@ -15,7 +15,7 @@ class CreateContract extends Migration
     {
         Schema::create('contract', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('text_contract')->nullable();
+            $table->longText('text_contract')->nullable();
 
             $table->timestamps();
         });
